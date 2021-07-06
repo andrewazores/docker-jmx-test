d708552d90c1313fe36af682f133e8b2e3116799
Andrew Azores <aazores@redhat.com>
2021-07-06 16:58:30 -0400

    Add test for illegal match expressions

--- src/test/java/itest/AutoRulesIT.java
+++ src/test/java/itest/AutoRulesIT.java
@@ -200,7 +200,9 @@ class AutoRulesIT extends ExternalTargetsTest {
         invalidRule.put("name", "Invalid_Rule");
         invalidRule.put("description", "AutoRulesIT automated rule");
         invalidRule.put("eventSpecifier", "template=Continuous,type=TARGET");
-        invalidRule.put("matchExpression", "target.annotations.cryostat.JAVA_MAIN=='es.andrewazor.demo.Main'");
+        invalidRule.put(
+                "matchExpression",
+                "target.annotations.cryostat.JAVA_MAIN=='es.andrewazor.demo.Main'");
         invalidRule.put("archivalPeriodSeconds", -60);
         invalidRule.put("preservedArchives", -3);
 
@@ -218,6 +220,30 @@ class AutoRulesIT extends ExternalTargetsTest {
 
     @Test
     @Order(3)
+    void testAddRuleThrowsWhenMatchExpressionIllegal() throws Exception {
+        CompletableFuture<JsonObject> postResponse = new CompletableFuture<>();
+        JsonObject invalidRule = new JsonObject();
+        invalidRule.put("name", "Invalid_Rule");
+        invalidRule.put("description", "AutoRulesIT automated rule");
+        invalidRule.put("eventSpecifier", "template=Continuous,type=TARGET");
+        invalidRule.put("matchExpression", "System.exit(1)");
+        invalidRule.put("archivalPeriodSeconds", -60);
+        invalidRule.put("preservedArchives", -3);
+
+        webClient
+                .post("/api/v2/rules")
+                .sendJsonObject(
+                        invalidRule,
+                        ar -> {
+                            if (assertRequestStatus(ar, postResponse)) {
+                                MatcherAssert.assertThat(
+                                        ar.result().statusCode(), Matchers.equalTo(400));
+                            }
+                        });
+    }
+
+    @Test
+    @Order(4)
     void testAddCredentials() throws Exception {
         CompletableFuture<JsonObject> response = new CompletableFuture<>();
         MultiMap form = MultiMap.caseInsensitiveMultiMap();
@@ -243,7 +269,7 @@ class AutoRulesIT extends ExternalTargetsTest {
     }
 
     @Test
-    @Order(4)
+    @Order(5)
     void testNewContainerHasRuleApplied() throws Exception {
         CONTAINERS.add(
                 Podman.run(
@@ -292,7 +318,7 @@ class AutoRulesIT extends ExternalTargetsTest {
     }
 
     @Test
-    @Order(5)
+    @Order(6)
     void testRuleCanBeDeleted() throws Exception {
         CompletableFuture<JsonObject> response = new CompletableFuture<>();
         webClient
@@ -314,7 +340,7 @@ class AutoRulesIT extends ExternalTargetsTest {
     }
 
     @Test
-    @Order(6)
+    @Order(7)
     void testCredentialsCanBeDeleted() throws Exception {
         CompletableFuture<JsonObject> response = new CompletableFuture<>();
         webClient
